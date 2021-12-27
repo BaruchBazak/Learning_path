de02c460fc90de09e7d9ad5ecec838611045ac32
baruchbaz <38757835+BaBa4@users.noreply.github.com>
2021-12-27 20:07:15 +0200

    Exercise 2

---
+++ Python/Exercise/E2.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to display the current date and time.
+Sample Output :
+Current date and time :
+2014-07-05 14:34:14
+"""
+import datetime
+date = datetime.datetime.now()
+print("Current date and time :\n{0}".format(date.strftime("%Y-%m-%d %X")))
