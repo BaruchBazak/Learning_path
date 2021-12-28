a499e051f7175e9acfa426767790a04ca8d98612
BaruchBazak <ubuntu@ip-172-31-23-58.eu-central-1.compute.internal>
2021-12-28 20:16:25 +0000

    test2

--- test_for_pytest.py
+++ test_for_pytest.py
@@ -1,9 +1,9 @@
 def test_uppercase():
     assert "loud noises".upper() == "LOUD NOISds"
 
-    
+
 def test_reversed():
-    assert list(reversed([1, 2, 3, 4])
+    assert list(reversed([1, 2, 3, 4]) == [4, 3, 2, 1]
 
 
 def test_name(name):
