ae45a18c5c71cde4b57edc7a44b9fbdf34eb6b66
BaruchBazak <ubuntu@ip-172-31-23-58.eu-central-1.compute.internal>
2021-12-28 23:35:55 +0000

    fix for github actions

--- test_for_pytest.py
+++ test_for_pytest.py
@@ -6,11 +6,3 @@ def test_reversed():
     assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
 
 
-def test_name(name):
-	return name[2]
-
-
-def test_name_check():
-	assert test_name("baruch") == "r"
-
-
