27ff086064e31fdb8d06a8379a7a92b81f511727
BaruchBazak <ubuntu@ip-172-31-23-58.eu-central-1.compute.internal>
2021-12-29 15:22:06 +0000

    backup

--- bash_python/remote.sh
+++ bash_python/remote.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo -e "Hello\nI will run ps command and after that, execute project.py"
-ps aux | grep "tcp"
-echo -e "\n[execute project.py]"
-python3 project.py
+echo -e "Menu:\n1.Print 1-10\n2.print num ** 2\n"
+read choice
+if [ $choice == 1 ];
+then 
+	echo -e "\n[execute project.py]"
+	python3 project.py
+elif [ $choice == 2 ];
+then
+	echo -e "[Execute another python file]"
+	python3 project2.py
+else
+	echo -e "dont know what do with your choice : $choice"
+fi
 echo -e "GoodBye.."
